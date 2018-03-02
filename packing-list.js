af36d795d01d3cbb3614be5d9aae7f64f1af4438
Steven Grewal <steven.grewal@gmail.com>
2018-03-02 23:38:50 +0000

    Adds looping exercises

---
+++ packing-list.js
@@ -0,0 +1,6 @@
+var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kichen stuff to pack:");
+for (var i =0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
